2e3123f5ffd1e944c5a5d49059ef22d21ff840ff
dfonsec <dfonseca@hmc.edu>
2025-01-08 17:28:01 -0500

    First version of working scraper

--- scrapers/psu_scraper.py
+++ scrapers/psu_scraper.py
@@ -1,30 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
 import re
-import undetected_chromedriver as uc
 import pandas as pd
 import time
 
 SEARCH_FIELD = '//*[@id="mat-input-0"]'
 SEARCH_BUTTON = '//*[@id="app-root"]/main/div/dir-directory/section/mat-card/mat-card-content/dir-search-bar/section/mat-form-field/div[1]/div/div[3]/button[2]'
-NEXT_BUTTON = '//*[@id="app-root"]/main/div/dir-directory/section/mat-card/mat-card-content/div/div[2]/pagination-controls/pagination-template/nav/ul/li[10]/a'
-# LETTERS = ['a', 'b', 'c', 'd', 'e', 
-#            'f', 'g', 'h', 'i', 'j', 
-#            'k', 'l', 'm', 'n', 'o', 
-#            'p', 'q', 'r', 's', 't', 
-#            'u', 'v', 'w', 'x', 'y', 'z']
-
-LETTERS = ['a', 'b']
+NEXT_BUTTON = '//pagination-controls//li/a[contains(text(), "Next")]'
+LETTERS = ['a', 'b', 'c', 'd', 'e', 
+           'f', 'g', 'h', 'i', 'j', 
+           'k', 'l', 'm', 'n', 'o', 
+           'p', 'q', 'r', 's', 't', 
+           'u', 'v', 'w', 'x', 'y', 'z']
 URL = "https://directory.psu.edu/"
 
 
 def crawler():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Run Chrome in headless mode
-    chrome_options.add_argument("start-maximized")
-    driver = uc.Chrome(Options=chrome_options)
+    chrome_options.add_argument("--headless")  
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get(URL)
     
     result= []
@@ -32,9 +28,7 @@ def crawler():
     for letter in LETTERS:
         print(f"Scraping data for letter: {letter}")
         data = scrape_page_data(driver, letter)
-        result.extend(data)  # Accumulate results across letters
-        
-    driver.quit()
+        result.extend(data)  
 
     return result
     
@@ -58,8 +52,8 @@ def scrape_page_data(driver, input):
                     panel_idx += 1
                     continue
                 
-                print(panel_idx)
-                click_button(driver, f'//*[@id="mat-expansion-panel-header-{panel_idx}"]/span[2]')
+                dropdown_button = panel.find_element(By.XPATH, './/span[contains(@class, "mat-expansion-indicator")]')
+                dropdown_button.click()
                 time.sleep(1)
                 data = get_panel_data(panel, panel_idx)
                 result_list.append(data)
@@ -68,13 +62,16 @@ def scrape_page_data(driver, input):
                 next_button = driver.find_element(By.XPATH, NEXT_BUTTON)
                 if next_button.is_enabled():
                     click_button(driver, NEXT_BUTTON)
-                    time.sleep(2)  # Wait for the next page to load
+                    time.sleep(2) 
                 else:
                     print("No more pages.")
                     break
             except TimeoutError:
                 print("Timeout Error")
                 break
+            except NoSuchElementException:
+                print("Next button not found on page, terminating process.")
+                return result_list
     
         except Exception as e:
                 print(f"Unexpected error: {e}")
@@ -112,14 +109,20 @@ def is_student(panel):
 
 def get_panel_data(panel, panel_idx):
     panel_name = panel.find_element(By.CLASS_NAME, "description-label").text
+    panel_email = panel.find_element(By.XPATH, './/div/section[1]/div[1]/dir-row-display[3]/section/div/a').text
+    panel_title = panel.find_element(By.XPATH, './/div/section[1]/div[2]/dir-row-display[1]/section/div/span').text
+    panel_campus = panel.find_element(By.XPATH, './/div/section[1]/div[2]/dir-row-display[3]/section/div/span').text
     
-    print("PANEL IDX IS CURRENTLY", panel_idx)
-    panel_email = panel.find_element(By.XPATH, f'//*[@id="cdk-accordion-child-{panel_idx}"]/div/section[1]/div[1]/dir-row-display[3]/section/div/a').text
-    
-    return {panel_name, panel_email}
+    return {'Name': panel_name, 'Email': panel_email, 'Title': panel_title, 'Campus': panel_campus}
 
 def main():
-    return crawler()
+    data_list = crawler()
+    if data_list:
+        data_df = pd.DataFrame(data_list, columns=['Name', 'Email', 'Title', 'Campus'])
+        data_df.to_excel('PSU_DATA.xlsx')
+    else:
+        print("No data scraped.")
+    return 
     
     
 if __name__ == "__main__":
