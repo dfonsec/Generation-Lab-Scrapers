a2da278e8b9f47c4205c926376282f86b00243fd
dfonsec <dfonseca@hmc.edu>
2025-01-14 20:05:49 -0500

    Finished final version of scraper

--- scrapers/mst_scraper.py
+++ scrapers/mst_scraper.py
@@ -12,39 +12,40 @@ def run_script(queries):
     session = requests.Session()
     data = []
     for query in queries:
+        print("Beginning to scrape:", query)
         init_url = f'https://campdir.apps.mst.edu/cgi-bin/cgiwrap/campdir/directory.pl'
         response = get_data(init_url, "P", session, query)
         anchors = get_profile_links(response)
-
+        page_data = get_user_data(anchors, session)
+        
+        data.extend(page_data)
     
+    return data
+        
+
 
 def get_data(url, req_type, session, query=None):
     
-    if req_type == "G":
-        response = session.get(url)
+    try:
+        if req_type == "G":
+            response = session.get(url)
+        elif req_type == "P":
+            data = configure_data(query)
+            response = session.post(url, data=data)
+    except:
+        print("Response failed:", response.status_code)
         
-    elif req_type == "P":
-        data = configure_data(query)
-        response = session.post(url, data=data)
     
     return response
 
 def configure_data(query):
-    
-    data = {"name": query,
-            "submit": "Search"
-            }
-    
-    return data
+    return {"name": query, "submit": "Search"}
 
 def get_profile_links(response):
+    
     soup = BeautifulSoup(response.text, 'html.parser')
-    
     pattern = re.compile(r'^directory\.pl\?mode=detail(&amp;|&)id=\w+\s*$')
-
-    
     anchors = soup.find_all('a', href=pattern)
-    
     return anchors
 
 def get_user_data(anchors, session):
@@ -52,47 +53,54 @@ def get_user_data(anchors, session):
     result = []
     base_url = "https://campdir.apps.mst.edu/cgi-bin/cgiwrap/campdir/"
     for anchor in anchors:
-        
-        href = anchor.get("href")
+        href = anchor.get("href").strip()
         curr_url = base_url + href
         user_response = get_data(curr_url, "G", session, None)
-        result.append(parse_user_data(user_response))
+        soup = BeautifulSoup(user_response.text, 'html.parser')
+        
+        if not is_student(soup):
+            continue
+        parsed_response = parse_user_data(soup)
+        result.append(parsed_response)
         
     return result
 
-def parse_user_data(user_response):
-    
-    soup = BeautifulSoup(user_response.text, 'html.parser')
+def parse_user_data(soup):
     table = soup.find("table")
     fonts = table.find_all("font")
+    
+    try:
+        name = fonts[0].get_text()
+    except:
+        name = None
+        
+    try:
+        email = fonts[-1].find("a").get_text()
+    except:
+        email = None
 
-    name = fonts[0].get_text
+    return {"Name": name, "Email": email}
+
+
+def is_student(soup):
     
-    email = fonts[-1].find("a").get_text()
+    div = soup.find("table")
+    category_elem = div.find("u")
+    elem_text = category_elem.get_text()
+    print(elem_text)
     
-    user_data = {"Name": name,
-                 "Email": email}
+    if elem_text[0] !=  "S":
+        return False
+    else:
+        return True
     
-    return user_data
     
 
 
 def main():
-    
-    session = requests.Session()
-    url = 'https://campdir.apps.mst.edu/cgi-bin/cgiwrap/campdir/directory.pl'
-    
-    response = session.post(url, data=data)
-    anchors = get_profile_links(response)
-    anchor = anchors[0]
-    curr_url = "https://campdir.apps.mst.edu/cgi-bin/cgiwrap/campdir/directory.pl?mode=detail&amp;id=mia5xb" 
-    user_response = get_data(curr_url, "G", session, None)
-    data = BeautifulSoup(user_response.text, 'html.parser')
-    table = data.find("table")
-    fonts = table.find_all("font")
-    print(fonts[0].get_text())
-    print(fonts[-1].find("a").get_text())
-
+    final = run_script(queries)
+    print(final)
+    print(len(final))
     return 
 
 if __name__ == "__main__":
