518b32e4b5413a428125e7669d779f1c91ba8fe8
dfonsec <dfonseca@hmc.edu>
2025-01-15 21:07:12 -0500

    saving work

--- scrapers/utc_scraper.py
+++ scrapers/utc_scraper.py
@@ -8,17 +8,38 @@ from bs4 import BeautifulSoup
 # Parse response 
 
 
+def replace_amp(input_string):
+    """
+    Replaces all occurrences of '&amp;' in the input string with '&'.
+
+    Parameters:
+        input_string (str): The input string to be processed.
+
+    Returns:
+        str: The processed string with '&amp;' replaced by '&'.
+    """
+    return input_string.replace("&amp;", "&")
+
+
 def main():
     session = requests.Session()
-    url = 'https://people.utc.edu/eGuide/servlet/eGuide?User.context=hmoxQmrwimHi&Action=List.get&Object.uid=USER&max=1&Search.attributes=,LASTNAME,EDUPERSONNICKNAME,EDUPERSONPRIMARYAFFILIATION&Primary.sortkey=LASTNAME&Secondary.sortkey=EDUPERSONNICKNAME'
+    anchor = "eGuide?User.context=onjsQmtnmgJu&amp;Action=Detail.get&amp;User.dn=cn%3dzql389%2cou%3dUsers%2co%3dutc&amp;Directory.uid=people&amp;Object.uid=USER"
+    user_url = f"https://people.utc.edu/eGuide/servlet/{replace_amp(anchor)}" 
+    print(user_url)
     data = {"attr1": "LASTNAME", "crit1": "sw", "val1": "b", "x": "43", "y": "23"}
-    response = session.post(url, data=data)
-    print(response.status_code)
+    # response = session.post(user_url, data=data)
+    response_user = session.get(user_url)
+    print(response_user.status_code)
     
-    soup = BeautifulSoup(response.text, 'html.parser')
-    soup.find("table", {"id": "resultTable"})
-    table_rows = soup.find_all("tr")
-    print(table_rows)
+    soup = BeautifulSoup(response_user.text, 'html.parser')
+    name = soup.find("td", {"class": "formhead2"})
+    values = soup.find_all("td", {"class": "ValueText"})
+    data = []
+    data.append(' '.join(name.get_text().split()))
+    data.extend([value.get_text().strip() for value in values])
+    
+    print(data)
+    
     
     return
 
