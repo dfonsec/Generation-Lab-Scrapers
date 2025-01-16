ae36d1e3e86f75b5f22be41e9ce83bcccf2b720c
dfonsec <dfonseca@hmc.edu>
2025-01-15 20:25:55 -0500

    Adding basic functionality

---
+++ scrapers/utc_scraper.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():
+    session = requests.Session()
+    url = 'https://people.utc.edu/eGuide/servlet/eGuide?User.context=hmoxQmrwimHi&Action=List.get&Object.uid=USER&max=1&Search.attributes=,LASTNAME,EDUPERSONNICKNAME,EDUPERSONPRIMARYAFFILIATION&Primary.sortkey=LASTNAME&Secondary.sortkey=EDUPERSONNICKNAME'
+    data = {"attr1": "LASTNAME", "crit1": "sw", "val1": "b", "x": "43", "y": "23"}
+    response = session.post(url, data=data)
+    print(response.status_code)
+    
+    soup = BeautifulSoup(response.text, 'html.parser')
+    soup.find("table", {"id": "resultTable"})
+    table_rows = soup.find_all("tr")
+    print(table_rows)
+    
+    return
+
+
+
+if __name__ == "__main__":
+    main()
