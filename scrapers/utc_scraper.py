b9120d8e40a6978931208266e1f62e23a0443daf
dfonsec <dfonseca@hmc.edu>
2025-01-17 23:21:47 -0500

    Completed parse_anchor and extract_urls functions

--- scrapers/utc_scraper.py
+++ scrapers/utc_scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 # Run script with a bunch of queries
 
@@ -8,37 +9,49 @@ from bs4 import BeautifulSoup
 # Parse response 
 
 
-def replace_amp(input_string):
-    """
-    Replaces all occurrences of '&amp;' in the input string with '&'.
 
-    Parameters:
-        input_string (str): The input string to be processed.
+# For each query, retrieve the entire list of hrefs that lead us directly to user links
+    # build a mass link of user links
+    # retrieve the user info via get requests
+    
 
-    Returns:
-        str: The processed string with '&amp;' replaced by '&'.
-    """
-    return input_string.replace("&amp;", "&")
 
+def extract_urls(soup):
+    # Assuming soup is the entire html
+    
+    # Obtain only the rows that contain data
+    links = []
+    rows = soup.find_all('tr', {"id": re.compile(r'^F')})
+    base_url = 'https://people.utc.edu/eGuide/servlet/'
+    result = [base_url + parse_anchor(row) for row in rows]
+    
+    return result
+  
+def parse_anchor(row):
+    anchor_url = row.get("onclick")
+    match = re.search(r"getDetail\('([^']+)'", anchor_url)
+    
+    if match:
+        anchor_link = match.group(1)
+        
+    return anchor_link
 
 def main():
     session = requests.Session()
-    anchor = "eGuide?User.context=onjsQmtnmgJu&amp;Action=Detail.get&amp;User.dn=cn%3dzql389%2cou%3dUsers%2co%3dutc&amp;Directory.uid=people&amp;Object.uid=USER"
-    user_url = f"https://people.utc.edu/eGuide/servlet/{replace_amp(anchor)}" 
-    print(user_url)
-    data = {"attr1": "LASTNAME", "crit1": "sw", "val1": "b", "x": "43", "y": "23"}
-    # response = session.post(user_url, data=data)
-    response_user = session.get(user_url)
-    print(response_user.status_code)
+    url = 'https://people.utc.edu/eGuide/servlet/eGuide?User.context=qtlsRhrljmKm&Action=List.get&Object.uid=USER&max=400&Search.attributes=,LASTNAME,EDUPERSONNICKNAME,EDUPERSONPRIMARYAFFILIATION&Primary.sortkey=LASTNAME&Secondary.sortkey=EDUPERSONNICKNAME'
+    data = {"attr1": "LASTNAME", "crit1": "sw", "val1": "a", "x": "43", "y": "23"}
+    response = session.post(url, data=data)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    rows = extract_urls(soup)
+    print(rows[:5])
+    # soup = BeautifulSoup(response_user.text, 'html.parser')
+    # name = soup.find("td", {"class": "formhead2"})
+    # values = soup.find_all("td", {"class": "ValueText"})
+    # data = []
+    # data.append(' '.join(name.get_text().split()))
+    # data.extend([value.get_text().strip() for value in values])
     
-    soup = BeautifulSoup(response_user.text, 'html.parser')
-    name = soup.find("td", {"class": "formhead2"})
-    values = soup.find_all("td", {"class": "ValueText"})
-    data = []
-    data.append(' '.join(name.get_text().split()))
-    data.extend([value.get_text().strip() for value in values])
-    
-    print(data)
+    # print(data)
     
     
     return
